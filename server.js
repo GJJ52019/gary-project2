334d67f948fa2e0a95071213f37448d59857a35c
gjj52019 <garyjjenks@verizon.net>
2021-10-04 13:48:52 -0400

    baseplates and files needed added as well as base functionality achieved for login page

--- server.js
+++ server.js
@@ -1,60 +1,74 @@
-require('dotenv').config()
-
-//___________________
 //Dependencies
 //___________________
 const express = require('express');
-const methodOverride = require('method-override');
-const mongoose = require ('mongoose');
 const app = express();
-const db = mongoose.connection;
+require('dotenv').config();
+const mongoose = require ('mongoose');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
 //___________________
 //Port
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 //___________________
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
 
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 // Error / success
+const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongod connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongod disconnected'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 //___________________
 //Middleware
 //___________________
 
 //use public folder for static assets
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));//requires express session
+    
+app.use(methodOverride('_method'));
 
 
 //___________________
 // Routes
 //___________________
 //localhost:3000
-app.get('/' , (req, res) => {
-  res.send('Hello World!');
-});
+const userController = require('./controllers/users');
+app.use('/users', userController);
 
+const sessionsController = require('./controllers/sessions');
+    app.use('/sessions', sessionsController);
+
+    // Routes / Controllers
+app.get('/', (req, res) => {
+	res.render('index.ejs', {
+		currentUser: req.session.currentUser
+	});
+});
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log('express is listening on:', PORT));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
